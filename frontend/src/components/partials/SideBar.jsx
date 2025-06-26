d95588b3038a68a94a4b9fa15db7508212075be5
Shashank Gupta <shashankg2705@gmail.com>
2025-06-26 13:57:43 +0530

    Sidebar

--- frontend/src/components/partials/SideBar.jsx
+++ frontend/src/components/partials/SideBar.jsx
@@ -3,21 +3,46 @@ import { Link } from 'react-router-dom'
 
 const SideBar = ( {focus} ) => {
   return (
-    <div className='bg-[#1f2937] h-screen !text-white'>
-      <div>
-        <i className="fas fa-wallet fa-2xl text-blue-500"></i>
-        <p>Finance Buddy</p>
+    <div className='flex flex-col bg-[#1f2937] gap-3 h-screen p-4'>
+      <div className='flex items-center gap-2 mb-6'>
+        <i className="fas fa-wallet text-2xl text-blue-500"></i>
+        <p className='font-bold text-2xl text-white leading-none m-0'>Finance Buddy</p>
       </div>
-      <div>
-        <Link>Dashboard</Link>
-        <Link>Transactions</Link>
-        <Link>Income/Budget</Link>
-        <Link>Investments</Link>
-        <Link>Loans</Link>
-        <Link>Goals</Link>
-        <Link>Categories</Link>
-        <Link>Reports</Link>
-        <Link>Settings</Link>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-chart-pie" style={{ color: '#ffffff' }} />
+        <Link to='/home' className='text-white !no-underline'>Dashboard</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-arrow-right-arrow-left" style={{ color: '#ffffff' }} />
+        <Link to='/transactions' className='text-white !no-underline'>Transactions</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-piggy-bank" style={{ color: '#ffffff' }} />
+        <Link to='/incomeBudget' className='text-white !no-underline'>Income/Budget</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-chart-line" style={{ color: '#ffffff' }} />
+        <Link to='/investments' className='text-white !no-underline'>Investments</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-hand-holding-dollar" style={{ color: '#ffffff' }} />
+        <Link to='/loans' className='text-white !no-underline'>Loans</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-bullseye" style={{ color: '#ffffff' }} />
+        <Link to='/goals' className='text-white !no-underline'>Goals</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-tags" style={{ color: '#ffffff' }} />
+        <Link to='/categories' className='text-white !no-underline'>Categories</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-file-export" style={{ color: '#ffffff' }} />
+        <Link to='/reports' className='text-white !no-underline'>Reports</Link>
+      </div>
+      <div className='flex items-center gap-2 opacity-80 hover:opacity-100'>
+        <i className="fa-solid fa-gear" style={{ color: '#ffffff' }} />
+        <Link to='/settings' className='text-white !no-underline'>Settings</Link>
       </div>
     </div>
   )
